1f19aca87422f6abe3156469bc40c44eebf43461
shujaat.ali <shujaat.ali@ihsinformatics.com>
2018-10-25 11:05:52 +0500

    First commit - complete the entity classes - Repository Interface - Services Class with curd methods - create Devices Controller and test

---
+++ src/main/java/com/device/manager/devicemanager/controllers/EmployeesController.java
@@ -0,0 +1,13 @@
+package com.device.manager.devicemanager.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EmployeesController {
+	
+	
+	
+	
+
+}
